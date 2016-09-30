67f64ed1549669008c5175c8d16908c4124bfd7e
liuhongnian <liuhongnian16@gmail.com>
2016-09-30 14:51:59 +0800

    初始化git仓库

---
+++ AixCategorys/AixCategorys/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AixCategorys
+//
+//  Created by liuhongnian on 16/9/30.
+//  Copyright © 2016年 liuhongnian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AixCategorys/AixCategorys/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AixCategorys
+//
+//  Created by liuhongnian on 16/9/30.
+//  Copyright © 2016年 liuhongnian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
