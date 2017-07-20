ebeb592920008a0bfd5ed806d5803a86433cdfea
liuhongnian <liuhongnian16@gmail.com>
2017-07-20 16:11:52 +0800

    no message

---
+++ AixCategorys/AixCategorys/FoundationKit/NSDictionary+AixCategory.h
@@ -0,0 +1,15 @@
+//
+//  NSDictionary+AixCategory.h
+//  AixCategorys
+//
+//  Created by liuhongnian on 12/1/16.
+//  Copyright © 2016 liuhongnian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (AixCategory)
+
+- (nullable NSString *)toJsonString;
+
+@end
---
+++ AixCategorys/AixCategorys/FoundationKit/NSDictionary+AixCategory.m
@@ -0,0 +1,26 @@
+//
+//  NSDictionary+AixCategory.m
+//  AixCategorys
+//
+//  Created by liuhongnian on 12/1/16.
+//  Copyright © 2016 liuhongnian. All rights reserved.
+//
+
+#import "NSDictionary+AixCategory.h"
+
+@implementation NSDictionary (AixCategory)
+
+- (NSString *)toJsonString
+{
+    if ([NSJSONSerialization isValidJSONObject:self]) {
+        NSError *error ;
+        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:self options:0 error:&error];
+        if (!error) {
+            NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+            return jsonString;
+        }
+    }
+    return nil;
+}
+
+@end
