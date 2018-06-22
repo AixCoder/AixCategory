a88f94c1fe07db42351f7dfb030f8d72c2965992
liuhongnian <liuhongnian16@gmail.com>
2018-06-22 17:33:17 +0800

    no message

---
+++ AixCategorys/AixCategorys/AixCategorys/QuartzCore/CALayer+AixCategory.m
@@ -0,0 +1,18 @@
+//
+//  CALayer+AixCategory.m
+//  AixCategorys
+//
+//  Created by liuhongnian on 2018/6/22.
+//  Copyright © 2018年 liuhongnian. All rights reserved.
+//
+
+#import "CALayer+AixCategory.h"
+
+@implementation CALayer (AixCategory)
+
+- (void)setXBorderColor:(UIColor *)XBorderColor
+{
+    self.borderColor = XBorderColor.CGColor;
+}
+
+@end
